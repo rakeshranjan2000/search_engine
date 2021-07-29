e2accc5160ab3ad99d7f3ef891978caffc124b93
rakeshranjan2000 <76446617+rakeshranjan2000@users.noreply.github.com>
2021-07-29 23:10:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+import string
+import nltk
+from nltk.tokenize import TreebankWordTokenizer
+from request import ques
+from nltk.stem.porter import PorterStemmer
+
+STEMMER = PorterStemmer()
+REMOVE_PUNCTUATION_TABLE = str.maketrans({x: None for x in string.punctuation})
+TOKENIZER = TreebankWordTokenizer()
+def tokenize_and_stem(s):
+    return [STEMMER.stem(t) for t 
+            in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    x=request.form['query']
+    ans=ques(x)
+    return render_template('index.html', your_list=ans)
+
+if __name__ == "__main__":
+    app.run(debug=True)
