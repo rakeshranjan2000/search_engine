805850281fffc22ca6804606135bb950f9cf2638
rakeshranjan2000 <76446617+rakeshranjan2000@users.noreply.github.com>
2021-08-10 23:24:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,25 +13,20 @@ TOKENIZER = TreebankWordTokenizer()
 
 
 app = Flask(__name__)
+def tokenize_and_stem(s):
+    return [STEMMER.stem(t) for t in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def tokenize_and_stem(s):
-    return [STEMMER.stem(t) for t in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
 
 @app.route('/predict',methods=['POST'])
 def predict():
     x=request.form['query']
-    #cv=pickle.load(open('converting_matrix.pkl','rb'))
-    #doc_vectors=pickle.load(open('doc_vectors.pkl','rb'))
-    #ans=ques(x,cv,doc_vectors)
-    ans=[]
-    y={"link":"check",
-       "text":"rakesh"
-      }
-    ans.append(y)
+    cv=pickle.load(open('converting_matrix.pkl','rb'))
+    doc_vectors=pickle.load(open('doc_vectors.pkl','rb'))
+    ans=ques(x,cv,doc_vectors)
     return render_template('index.html', your_list=ans)
 
 if __name__ == "__main__":
