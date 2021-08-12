3cbe9c06946ac6937fee72c72117170e98ce915a
rakeshranjan2000 <76446617+rakeshranjan2000@users.noreply.github.com>
2021-08-12 11:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,7 @@ import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 import string
 import nltk
-from nltk.tokenize import TreebankWordTokenizer
 from helper import ques
-from nltk.stem.porter import PorterStemmer
-STEMMER = PorterStemmer()
-REMOVE_PUNCTUATION_TABLE = str.maketrans({x: None for x in string.punctuation})
-TOKENIZER = TreebankWordTokenizer()
-from sklearn.feature_extraction.text import TfidfVectorizer
-def tokenize_and_stem(s):
-    return [STEMMER.stem(t) for t in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
-
-vectorizer = TfidfVectorizer(tokenizer=tokenize_and_stem, stop_words='english')
 
 app = Flask(__name__)
 
@@ -26,9 +16,7 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     x=request.form['query']
-    cv=pickle.load(open('new_converting_matrix.pkl','rb'))
-    doc_vectors=pickle.load(open('new_doc_vectors.pkl','rb'))
-    ans=ques(x,cv,doc_vectors)
+    ans=ques(x)
     return render_template('index.html', your_list=ans)
 
 if __name__ == "__main__":
